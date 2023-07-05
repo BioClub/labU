0db1974e2aff18fda7b1224a803f0fd2656481a9
Georg Tremmel <g@trembl.org>
2023-07-05 20:19:53 +0900

    Added 404 page

--- 404.php
+++ 404.php
@@ -1,13 +1,5 @@
 <?php get_header(); ?>
 
-      <div class="col_wrap col_full">
-        <div class="col_box header_box">
-          <div class="title_head">Médias Pionniers:</div>
-          <div class="title_subhead">404</div>
-        </div><!-- end col_box -->
-      </div><!-- end col_wrap -->
-    </div><!-- end col_row -->
-
-  </div><!-- end header col_container -->
+404 - Page Not Found
 
 <?php get_footer(); ?>
