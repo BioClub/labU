ff8cb14b5106936aeec597dfb589c3e38637c6c2
Georg Tremmel <g@trembl.org>
2023-07-06 09:31:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 <article class="ph3 ph5-ns pv5 ">
   <h1 class="f1">
-    <?php the_title(); ?>
+    <?php the_title(); ?> 
   </h1>
   <div class="lh-copy">
     <?php the_content(); ?>
