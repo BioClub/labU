e241f07d170d651ee971ef49172b807f9ad6b08b
Georg Tremmel <g@trembl.org>
2023-07-13 23:05:47 +0900

    Page Content Display

--- templates/_main.php
+++ templates/_main.php
@@ -72,19 +72,7 @@ foreach($languages as $language) :
 
 <!-- article -->
 <article id="content" class="pv5 ">
-  <h1 class="f1">
-    <?php echo $page->title; ?>
-<?php if($page->editable()): ?><a href='<?php echo $page->editUrl(); ?>'>&nbsp;&nbsp;&nbsp;</a></p><?php endif; ?>
-  </h1>
-  <div class="lh-copy">
-<?php echo $page->content; ?>
-  </div>
-  <div id="byline" class="pb2">
-    Author: <?php $createdUser = $page->createdUser; echo $createdUser->user_display_name; ?><br />
-    Published: <?php echo date('l jS \of F Y h:i:s A', $page->published); ?>, 
-    Last Update: <?php echo date('l jS \of F Y h:i:s A', $page->modified); ?>
-    <?php if($page->editable()): ?><a href='<?php echo $page->editUrl(); ?>'>Edit</a></p><?php endif; ?>
-  </div>
+
 </article>
 
 
--- templates/home.php
+++ templates/home.php
@@ -7,10 +7,17 @@
 ?>
 
 <div id="content">
-  
-  <?php echo $page->content; ?>
-  
-  
-  <?php echo $page->publication_date; ?>
-</div>	
-	
+   <h1 class="f1">
+     <?php echo $page->title; ?>
+ <?php if($page->editable()): ?><a href='<?php echo $page->editUrl(); ?>'>&nbsp;&nbsp;&nbsp;</a></p><?php endif; ?>
+   </h1>
+   <div class="lh-copy">
+ <?php echo $page->content; ?>
+   </div>
+   <div id="byline" class="pb2">
+     Author: <?php $createdUser = $page->createdUser; echo $createdUser->user_display_name; ?><br />
+     Published: <?php echo date('l jS \of F Y h:i:s A', $page->published); ?>, 
+     Last Update: <?php echo date('l jS \of F Y h:i:s A', $page->modified); ?>
+     <?php if($page->editable()): ?><a href='<?php echo $page->editUrl(); ?>'>Edit</a></p><?php endif; ?>
+   </div>
+</div>
