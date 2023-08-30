9755848936c66d660d53effedb81e482dd131a3c
Georg Tremmel <g@trembl.org>
2023-08-30 20:02:33 +0900

    Update event.php

--- templates/event.php
+++ templates/event.php
@@ -4,14 +4,36 @@
 
 ?>
 
-<article id="content" class="pv5 ">
-  Event Template
+<article id="content" class="pv5">
   <h1 class="f1">
-    <?php echo $page->title; ?>
+    <?php if ($page->speaker_name) { echo $page->speaker_name; echo ": ";} ?><?=$page->title?>
   </h1>
+<?php
+  // Featured Image
+  $featured_image = false;
+  if ($page->featured_image) {
+    $featured_image = $page->featured_image->size(900, 600);
+  }
+?>
+  <div>
+    <img src="<?=$featured_image->url?>" />
+  </div>
+  
   <div class="lh-copy">
 <?php echo $page->content; ?>
   </div>
+  
+<?php
+/*
+
+  <div>Zoom: <a href="https://zoom.bioclub.tokyo">https://zoom.bioclub.tokyo</a></div>
+  
+  <div>Place: <a href="https://goo.gl/maps/4Xf5Ep7AFbbmt7uAA">BioClub Tokyo, FabCafe MTRL 2F</a></div>
+  <div>Fee: 無料</div>
+*/  
+  
+?>
+
   <div id="byline" class="pb2">
     Author: <?php $createdUser = $page->createdUser; echo $createdUser->user_display_name; ?><br />
     Published: <?php echo date('l jS \of F Y h:i:s A', $page->published); ?>, 
