f976cd4e32f998051667f3947aa4fbf6ac4016ea
Georg Tremmel <g@trembl.org>
2023-09-12 16:58:40 +0900

    Inventory

--- templates/inventory-item.php
+++ templates/inventory-item.php
@@ -2,13 +2,33 @@
 
 // Template file for pages using the “inventory-item” template
 
+
+
+
 ?>
 
 <article id="content" class="pv5">
-  <div class="f1 i">
-    Single Inventory Item
+  <div class="f1">
+    <?=$page->title?>
   </div>
   <div class="f3 lh-copy">
-
+    <?=$page->content?>
   </div>
+  <div class="f4 lh-copy">
+    <?=$page->user_reference->first()->user_display_name?>
+  </div>
+  <div class="f4 lh-copy">
+    <?=$page->inventory_usage->title?>
+  </div>
+  
+  
+  
+<?php if ($page->images->first()):
+    $img = $page->images->first();
+?>
+    <div class="tl">
+      <img src="<?=$img->url?>" />
+      <figcaption class="f6 silver"><?=$img->description?></figcaption>
+    </div>
+<?php endif; ?>
 </article>
--- templates/inventory.php
+++ templates/inventory.php
@@ -42,11 +42,5 @@ $page->children()->each(function($item) {
 ?>
       </tbody>
     </table>
-  
-  <div id="byline" class="pv5 moon-gray">
-    Author: <?php $createdUser = $page->createdUser; echo $createdUser->user_display_name; ?><br />
-    Published: <?php echo date('l jS \of F Y h:i:s A', $page->published); ?>, 
-    Last Update: <?php echo date('l jS \of F Y h:i:s A', $page->modified); ?>
-    <?php if($page->editable()): ?><a href='<?php echo $page->editUrl(); ?>'>Edit</a></p><?php endif; ?>
-  </div>
+
 </article>
