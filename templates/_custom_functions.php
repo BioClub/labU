38624dc80fc6e48b0fa293e274958302d2e3b4c9
Georg Tremmel <g@trembl.org>
2023-12-19 00:07:14 +0900

    Create _custom_functions.php
    for 404 error handling

---
+++ templates/_custom_functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// Custom Functions should only be included once, or else a "Cannot redeclare function" error is triggered when invoking wire404() [see members-overview.php]
+
+
+// Translation Helper
+function _e($str, $domain=null) {
+  echo __($str, $domain);
+}
+
+// Menu
+function displayMenu($menu, $name) {
+  $menuItems = $menu->getMenuItems($name, 2); // 2 -> return Object
+  foreach($menuItems as $item) {
+    echo "<a href='$item->url' class=''>$item->title</a> | \n";
+  }
+}
+
+?>
