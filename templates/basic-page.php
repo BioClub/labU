3c8527136e0685d0cdaf08899c691dca432c6c74
Georg Tremmel <g@trembl.org>
2023-12-19 00:07:41 +0900

    Update basic-page.php

--- templates/basic-page.php
+++ templates/basic-page.php
@@ -7,13 +7,15 @@
 <div id="content">
    <h2 class=""><?php echo $page->title; ?></h2>
    <div class="">
- <?php echo $page->content; ?>
+<?php echo $page->content; ?>
    </div>
+<!--
    <div id="byline" class="py-8 text-xs text-gray-300">
      Author: <?php $createdUser = $page->createdUser; echo $createdUser->user_display_name; ?><br />
      Published: <?php echo date('l jS \of F Y h:i:s A', $page->published); ?>, 
      Last Update: <?php echo date('l jS \of F Y h:i:s A', $page->modified); ?>
      <?php if($page->editable()): ?><a href='<?php echo $page->editUrl(); ?>'>Edit</a></p><?php endif; ?>
    </div>
+-->
 </div>
 
