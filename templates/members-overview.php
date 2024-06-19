ac7704090d9dd7edd57591f6def0b60e9f9353da
Georg Tremmel <g@trembl.org>
2024-06-19 21:22:08 +0900

    Styles for Member Overview Page

--- templates/members-overview.php
+++ templates/members-overview.php
@@ -6,17 +6,16 @@ $userPage = $input->urlSegmentStr;
 if (!$userPage):
 ?>
 
-<article id="content" class="">
+<article id="content" class="max-w-6xl mx-auto">
   <h1 class="mb-8 text-center">
     <?= __("BioClub Members") ?>
   </h1>
-  <div class="flex w-3/4 p-6 mx-auto text-lg font-medium">
+  <div class="text-lg font-medium pb-8">
     <?= $page->content ?>
   </div>
 
-  <div id="members">
+  <div id="members" class="grid gap-3 grid-cols-1 lg:grid-cols-2">
 <?php
-
 foreach ($users->find("template=user,roles=member") as $u){
   $user_image = false;
   if ($u->images->first()) {
@@ -24,12 +23,16 @@ foreach ($users->find("template=user,roles=member") as $u){
     $base = $urls->httpRoot;
     $user_image = "$base/site/assets/files/$u->id/$user_image";
   }
+  $background_color = ""; // default
+  if ($u->color) {
+    $background_color =  "style=\"background-color:$u->color\"";
+  }
 ?>
-    <a href="<?=$u->user_nice_url?>" class="user flex w-3/4 p-6 mx-auto">
+    <a href="<?=$u->user_nice_url?>" class="flex p-4">
 <?php if ($user_image): ?>
       <img class="flex-none w-16 h-16 rounded-full" src="<?=$user_image?>" />
 <?php else: ?>
-      <div class="flex-none w-16 h-16 rounded-full bg-slate-200"></div>
+      <div class="flex-none w-16 h-16 rounded-full bg-slate-200"<?=$background_color?>></div>
 <?php endif; ?>
       <div href="<?=$u->user_nice_url?>" class="flex-auto pl-6">
         <div class="text-2xl font-medium"><?=$u->user_display_name?></div>
