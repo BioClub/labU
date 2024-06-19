5abdccd532aeae89f9d593916432b7516a7304ca
Georg Tremmel <g@trembl.org>
2024-06-19 16:59:35 +0900

    Project Overview Styles

--- templates/projects-overview.php
+++ templates/projects-overview.php
@@ -2,28 +2,26 @@
 
 // Template file for pages using the “projects-overview” template
 
-
-
 ?>
 
 <article id="content" class="text-center">
-  <p class="text-4xl">BioClub Projects</p>
+  <h3 class="text-4xl pb-12 pt-8">BioClub Projects</h3>
 
-  
+  <div class="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 <?php 
-// All Projects
+// Get all Projects by date
 $page->children("sort=date")->each(function($project) {
   $img = false;
   if ($project->images->first()) {
-    $img = $project->images->first()->size(200, 200);
+    $img = $project->images->first()->size(480, 270);
   }
 ?>
 
-    <a href="<?=$project->url?>" class="flex font-medium items-center box hover:bg-gray-100 active:bg-pure-yellow">
+    <a href="<?=$project->url?>" class="rounded-2xl">
 <?php if ($img): ?>
-      <img class="flex-none w-16 h-16 rounded-full" src="<?=$img->url?>" />
+      <img class="w-full rounded-t-2xl" src="<?=$img->url?>" />
 <?php endif; ?>
-      <div class="pl-4 text-4xl"><?=$project->title?></div>
+      <div class="text-2xl"><?=$project->title?></div>
     </a>
 
     
@@ -31,6 +29,5 @@ $page->children("sort=date")->each(function($project) {
 });
 ?>
 
-
-  </div>
+  </div><!-- flex end -->
 </article>
