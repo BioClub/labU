00f5a7c22e6fcef7413f018d491b6e9a76846d3d
Georg Tremmel <g@trembl.org>
2024-06-27 16:49:09 +0900

    News Style

--- templates/news-overview.php
+++ templates/news-overview.php
@@ -4,34 +4,31 @@
 
 ?>
 
-<article id="content" class="">
+<article id="content" class="news">
   <h2 class="text-center pb-10"><?= _t("Latest News") ?></h2>
   
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-3">
 <?php 
 // Get News
-$page->children("sort=-event_date")->each(function($news) {
+$page->children("sort=-date")->each(function($news) {
   ?>
-  <a class="" href="<?=$news->url?>">
+  <a class="box" href="<?=$news->url?>">
     
     <div class="leading-8 mb-2">
-      <span class="text-ms rounded-full px-2 py-1"><?=$news->event_date?> JST</span>
+      <span class="text-ms px-2 py-1"><?=$news->date?> JST</span>
     </div>
     
 <?php if ($news->images->first()):
-  $img = $news->images->first()->size(1000, 333);
+  $img = $news->images->first()->size(960, 333);
 ?>
     <img src="<?=$img->url?>" class="w-full mb-3" />
 <?php endif; ?>
 
- 
     <h2 class="px-1">
       <?=$news->title?>
     </h2>
-
-
     <div class="px-1">
-      <?=$news->event_abstract?>
+      <?=$news->abstract?>
     </div>
   </a>
     
