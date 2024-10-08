0ef665c247c5ad71add02ac7ff9daa39abb26c95
Georg Tremmel <g@trembl.org>
2024-10-08 15:11:33 +0900

    Project Members

--- ready.php
+++ ready.php
@@ -13,3 +13,62 @@ if(!defined("PROCESSWIRE")) die();
  * copy of all ProcessWire API variables.
  *
  */
+
+
+// https://processwire.com/api/ref/pages/saved/
+
+/**
+ * Update/create Roles when Project Pages are saved
+ *
+ */
+$this->addHookAfter('Pages::saved', function(HookEvent $event) {
+
+  // Get the object the event occurred on, if needed
+  $pages = $event->object;
+
+
+  // Check if page is of template 'project'
+
+  // An 'after' hook can retrieve and/or modify the return value
+  //$return = $event->return;
+
+
+  // Get values of arguments sent to hook (if needed)
+  $page = $event->arguments(0);
+  $changes = $event->arguments(1);
+  $values = $event->arguments(2);
+
+  $rolePrefix = "role-for-project-";
+  if ("project" == $page->template) {
+    $this->message("Current Page is Project!");
+    $roles = roles();   // get roles
+    $roleName = $rolePrefix . $page->id;
+    $role = $roles->find("name=$roleName", array('findOne'=>true))[0]; // check if role exists, get first one
+    if ($role) {
+      $this->message("Found a Role: '$rolePrefix$role'");
+
+    } else { // if role does not exist, make it
+      $this->message("Did not find Role, making new one: $roleName");
+      $role = $roles->add($roleName);
+      $this->message("New Role created: $role");
+    }
+
+    // Add Role to each Project Member
+    if (count($page->user_reference) > 0) {
+      foreach($page->user_reference as $projectMember) {
+        $projectMember->addRole($roleName);
+        $projectMember->save();
+      }
+    }
+
+    // Update Role Permission
+  }
+
+  // $this->message($page);
+
+  /* Your code here, perhaps modifying the return value */
+
+  // Populate back return value, if you have modified it
+  //$event->return = $return;
+
+});
--- templates/project.php
+++ templates/project.php
@@ -11,26 +11,26 @@
     <?=$page->content?>
   </div>
 
-<?php 
+<?php
 // Show associates Project Members
 if ($page->user_reference->count > 0) {
 ?>
   <div class="my-4 text-center">
     <div class="text-2xl text-center">Project Members:</div>
     <div class="my-4">
-<?php 
+<?php
 
 $page->user_reference->each(function($u) {
   $m = wire('pages')->get('template=members-overview');  // $page not defined in functions: https://processwire.com/talk/topic/13776-why-is-pages-undefined/
 ?>
     <a href="<?=$m->url.$u->user_nice_url?>" class="rounded-full bg-gray-200 px-2 py-1"><?=$u->user_display_name?></a>
-<?php 
+<?php
 });
-?>  
+?>
     </div>
   </div>
 <?php
 }
 ?>
-  
+
 </article>
