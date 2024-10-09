452718265ffd2f9f13e98ca4b9cb810996a6c0f4
Georg Tremmel <g@trembl.org>
2024-10-09 15:50:05 +0900

    Updating users on save

--- ready.php
+++ ready.php
@@ -43,67 +43,84 @@ $this->addHookAfter('Pages::saved', function(HookEvent $event) {
 
   // If page template is project, clone template
   if ("project" == $page->template) {
-    $templates = templates();
-    $projectTemplate = $templates->get("project");
-    $this->message("Making new Template: $projectTemplate");
-    $newTemplateName = $templatePrefix . $page->id;
-    if (!$templates->get($newTemplateName)) {
-      $templates->clone($projectTemplate, $newTemplateName);
-      $this->message("New Template created: $newTemplateName");
+    $t = templates();
+    //$newTemplate = $t->get("project");
+    $newTemplate = $page->template;
+    $this->message("Making new Template: $newTemplate");
+    $name = $templatePrefix . $page->id;
+    if (!$t->get($name)) {
+      $t->clone($newTemplate, $name);
+
+      // Assign current Page to new Template
+      $page->template = $name;
+
+      // Add 'Alternate Template Filename'. New Projects should refer to 'project.php'
+      $page->template->altFilename = "project"; // Template file extension is assumed to be: .php
+
+      // Update Labels of Template
+      $enID = languages()->getLanguage("en")->id;
+      $page->template->label = "Project: " . $page->title;
+      $page->template["label$enID"] = "Project: " . $page->getLanguageValue("en", "title");
+      $page->template->save();
+      $page->save();
+
+      $this->message("New Template created: ". $page->template["label$enID"]);
+
+      // Roles
+      $roles = roles();
+      $roleName = $rolePrefix . $page->id;
+      $role = $roles->find("name=$roleName", array('findOne'=>true))[0]; // check if role exists, get first one
+      if ($role) {
+        $this->message("Found a Role: '$rolePrefix $role'");
+
+      } else { // if role does not exist, make it
+        $this->message("Making new Role: $roleName");
+        $role = $roles->add($roleName);
+        $this->message("New Role created: $role");
+      }
+
+      // Setting Persmissions at Role
+      // General "page-edit" checkbox
+      $role->addPermission("page-edit");
+      $role->save();
+      // Checkbox at "Edit Pages"
+      $page->template->addPermissionByRole('page-edit', $role);
+      $page->template->save();
+
+      // Add Role to each Project Member
+      if (count($page->user_reference) > 0) {
+        foreach($page->user_reference as $u) {
+          $this->message("Adding User $u->name to role $roleName");
+          $u->addRole($roleName);
+          $u->save();
+        }
+      }
+
     }
-
-    // Assign current Page to new Template
-    $page->template = $newTemplateName;
-
-    // Update Labels of Template
-    $enID = languages()->getLanguage("en")->id;
-    $page->template->label = "Project: " . $page->title;
-    $page->template["label$enID"] = "Project: " . $page->getLanguageValue("en", "title");
-    $page->template->save();
-    $page->save();
-
-    // Create New Role
   }
 
+  if (substr($page->template, 0, strlen($templatePrefix)) == $templatePrefix) {
+    // Update Users for Role when Project is saved.
 
-
-
-  if ("project-template-1037" == $page->template) {
-    $this->message("Current Page is Project: " . $page->id);
-    $roles = roles();   // get roles
     $roleName = $rolePrefix . $page->id;
-    $role = $roles->find("name=$roleName", array('findOne'=>true))[0]; // check if role exists, get first one
-    if ($role) {
-      $this->message("Found a Role: '$rolePrefix $role'");
 
-    } else { // if role does not exist, make it
-      $this->message("Did not find Role, making new one: $roleName");
-      $role = $roles->add($roleName);
-      $this->message("New Role created: $role");
+    // Remove Role from All Users. case: when a project removes a user
+    $users = users();
+    foreach($users as $u) {
+      $this->message("Removing User $u->name from $roleName.");
+      $u->removeRole($roleName);
+      $u->save();
     }
 
     // Add Role to each Project Member
     if (count($page->user_reference) > 0) {
       foreach($page->user_reference as $u) {
-        $this->message("Adding User $u to Role.");
+        $this->message("Adding User $u->name to $roleName.");
         $u->addRole($roleName);
         $u->save();
       }
     }
-
-    // Setting Persmissions at Role
-    // General "page-edit" checkbox
-    $role->addPermission("page-edit");
-    $role->save();
-
-    // Checkbox at "Edit Pages"
-    $page->template->addPermissionByRole('page-edit', $role);
-    $page->template->save();
   }
-
-
-
-
 
   // $this->message($page);
 
