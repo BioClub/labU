1fa0aeb5e0b383de20c74b42b15a69a54bf4457a
Georg Tremmel <g@trembl.org>
2024-12-11 12:10:19 +0900

    Added List of Related Events to Project Page
    No styling yet

--- templates/project.php
+++ templates/project.php
@@ -1,4 +1,4 @@
-<?php namespace ProcessWire; 
+<?php namespace ProcessWire;
 
 // Template file for pages using the “project” template
 
@@ -15,9 +15,9 @@
 // Show associates Project Members
 if ($page->user_reference->count > 0) {
 ?>
-  <div class="my-4 text-center">
-    <div class="text-2xl text-center">Project Members:</div>
-    <div class="my-4">
+  <div class="my-4 font-medium">
+    <div class="">Project Members:</div>
+    <div class="my-2">
 <?php
 
 $page->user_reference->each(function($u) {
@@ -28,6 +28,22 @@ $page->user_reference->each(function($u) {
 });
 ?>
     </div>
+  </div>
+  <div class="my-4 font-medium">
+    <div class=" ">Related Events:</div>
+    <ul>
+<?php
+
+// Search all events, that have this page in project_reference
+// $events = $pages->find("template=event, project_reference=$page, limit=10");
+$events = $pages->find("template=event, project_reference=$page");
+
+$events->each(function($e) {
+  echo '<li><a href="'.$e->url.'">'.$e->title.', '.$e->event_date.'</a></li>';
+});
+
+?>
+    </ul>
   </div>
 <?php
 }
