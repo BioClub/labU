169f459aed37579cafea770fdf364efae60a6be5
Georg Tremmel <g@trembl.org>
2025-03-31 12:37:56 +0900

    Assign Project Member edit access to the Project Page
    All Project Members from a Project Page get assigned Project Page-Edit permissions on save.

--- ready.php
+++ ready.php
@@ -15,117 +15,42 @@ if(!defined("PROCESSWIRE")) die();
  */
 
 
-// https://processwire.com/api/ref/pages/saved/
 
-/**
- * Update/create Roles when Project Pages are saved
- *
- */
+// Assign Project page-edit permission for Project Members
+// https://processwire.com/api/ref/pages/saved/
+// Update/create Roles when Project Pages are saved
 $this->addHookAfter('Pages::saved', function(HookEvent $event) {
 
   // Get the object the event occurred on, if needed
   $pages = $event->object;
 
-
-  // Check if page is of template 'project'
-
-  // An 'after' hook can retrieve and/or modify the return value
-  //$return = $event->return;
-
-
   // Get values of arguments sent to hook (if needed)
   $page = $event->arguments(0);
-  $changes = $event->arguments(1);
-  $values = $event->arguments(2);
+  // $changes = $event->arguments(1);
+  // $values = $event->arguments(2);
 
-  $rolePrefix = "role-for-project-";
-  $templatePrefix = "project-template-";
-
-  // If page template is project, clone template
+  // Check if page is of template 'project'
   if ("project" == $page->template) {
-    $t = templates();
-    //$newTemplate = $t->get("project");
-    $newTemplate = $page->template;
-    $this->message("Making new Template: $newTemplate");
-    $name = $templatePrefix . $page->id;
-    if (!$t->get($name)) {
-      $t->clone($newTemplate, $name);
+    //$this->message("Page $page is a Project Page");  // $this->message, $this->error
 
-      // Assign current Page to new Template
-      $page->template = $name;
-
-      // Add 'Alternate Template Filename'. New Projects should refer to 'project.php'
-      $page->template->altFilename = "project"; // Template file extension is assumed to be: .php
-
-      // Update Labels of Template
-      $enID = languages()->getLanguage("en")->id;
-      $page->template->label = "Project: " . $page->title;
-      $page->template["label$enID"] = "Project: " . $page->getLanguageValue("en", "title");
-      $page->template->save();
-      $page->save();
-
-      $this->message("New Template created: ". $page->template["label$enID"]);
-
-      // Roles
-      $roles = roles();
-      $roleName = $rolePrefix . $page->id;
-      $role = $roles->find("name=$roleName", array('findOne'=>true))[0]; // check if role exists, get first one
-      if ($role) {
-        $this->message("Found a Role: '$rolePrefix $role'");
-
-      } else { // if role does not exist, make it
-        $this->message("Making new Role: $roleName");
-        $role = $roles->add($roleName);
-        $this->message("New Role created: $role");
-      }
-
-      // Setting Persmissions at Role
-      // General "page-edit" checkbox
-      $role->addPermission("page-edit");
-      $role->save();
-      // Checkbox at "Edit Pages"
-      $page->template->addPermissionByRole('page-edit', $role);
-      $page->template->save();
-
-      // Add Role to each Project Member
-      if (count($page->user_reference) > 0) {
-        foreach($page->user_reference as $u) {
-          $this->message("Adding User $u->name to role $roleName");
-          $u->addRole($roleName);
-          $u->save();
-        }
-      }
-
-    }
-  }
-
-  if (substr($page->template, 0, strlen($templatePrefix)) == $templatePrefix) {
-    // Update Users for Role when Project is saved.
-
-    $roleName = $rolePrefix . $page->id;
-
-    // Remove Role from All Users. case: when a project removes a user
-    $users = users();
-    foreach($users as $u) {
-      $u->removeRole($roleName);
-      $u->save();
-    }
-
-    // Add Role to each Project Member
+    // List all Project Members from field 'user_reference'
     if (count($page->user_reference) > 0) {
       foreach($page->user_reference as $u) {
-        $this->message("Adding User $u->name to $roleName.");
-        $u->addRole($roleName);
+        // Empty Page Array
+        $u->editable_pages->removeAll();
+
+        // Find all Projects where $u is a member
+        $projects = $pages->find("template=project, user_reference=$u->id");
+        foreach($projects as $p) {
+          $u->editable_pages->add($p); // Add all User Project Pages to Editable Pages
+        }
+
+        // Save User
         $u->save();
+        //$this->message("User Editable Pages updated: " . $u->name . ", ". $u->editable_pages);
       }
     }
+
   }
-
-  // $this->message($page);
-
-  /* Your code here, perhaps modifying the return value */
-
-  // Populate back return value, if you have modified it
-  //$event->return = $return;
 
 });
