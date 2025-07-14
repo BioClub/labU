0b77ab9ea22d2eb1cfd4e308cac2027a3a8b83a5
Georg Tremmel <g@trembl.org>
2025-07-14 17:18:50 +0900

    Only show related Event pages, if they exist

--- templates/project.php
+++ templates/project.php
@@ -13,13 +13,12 @@
 
 <?php
 // Show associates Project Members
-if ($page->user_reference->count > 0) {
+if ($page->user_reference->count > 0):
 ?>
   <div class="my-4 font-medium">
     <div class=""><?= _t("Project Members:"); ?></div>
     <div class="my-2">
 <?php
-
 // WireArray Implode: https://processwire.com/talk/topic/5098-new-wirearray-api-additions-on-dev/
 echo $page->user_reference->implode(", ", function($u) {
   $m = wire('pages')->get('template=members-overview');  // $page not defined in functions: https://processwire.com/talk/topic/13776-why-is-pages-undefined/
@@ -29,23 +28,29 @@ echo $page->user_reference->implode(", ", function($u) {
 ?>
     </div>
   </div>
-  <div class="my-4 font-medium">
-    <div class=""><?= _t("Related Events:"); ?></div>
-    <ul>
 <?php
+endif;
 
 // Search all events, that have this page in project_reference
 // $events = $pages->find("template=event, project_reference=$page, limit=10");
 $events = $pages->find("template=event, project_reference=$page, sort=-event_date");
+
+// Only show related events, if they exist
+if ($events->count > 0):
+?>
+  <div class="my-4 font-medium">
+    <div class=""><?= _t("Related Events:"); ?></div>
+    <ul>
+<?php
 echo $events->implode(function($e) {
   return '<li><a href="'.$e->url.'">'.$e->title.', '.$e->event_date.'</a></li>';
 });
-
 ?>
     </ul>
   </div>
 <?php
-}
+endif;
 ?>
+
   </div><!-- end content -->
 </article>
